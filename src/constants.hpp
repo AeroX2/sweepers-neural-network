4b3c856ce0eece17145df9bd35958a9f92943724
AeroX2 <james@snoopyaustralia.com>
2016-05-10 19:31:25 +1000

    Added config.cpp and gitignore

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,28 +1,28 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+static int SCREEN_WIDTH = 640;
+static int SCREEN_HEIGHT = 480;
 
-const float FRAMERATE = 1000/60;
+static float FRAMERATE = 1000/60;
 
-const int POPULATION = 20;
-const int MINE_LEN = 40;
-const int EPOCH_TICK_OVER = 2000;
-const int HIT_DISTANCE = 5;
+static int POPULATION = 20;
+static int MINE_LEN = 40;
+static int EPOCH_TICK_OVER = 2000;
+static int HIT_DISTANCE = 5;
 
-const float MUTATION_CHANCE = 0.1;
-const float COMBINE_CHANCE = 0.7;
-const float MAX_PERTURB = 0.3;
+static float MUTATION_CHANCE = 0.1;
+static float COMBINE_CHANCE = 0.7;
+static float MAX_PERTURB = 0.3;
 
-const float CONTROL_SWEEPER_SPEED = 1.5;
-const float MAX_SWEEPER_TURN_RATE = 0.3;
-const float MAX_SWEEPER_SPEED = 2.0;
+static float CONTROL_SWEEPER_SPEED = 1.5;
+static float MAX_SWEEPER_TURN_RATE = 0.3;
+static float MAX_SWEEPER_SPEED = 2.0;
 
-const int BRAIN_INPUT_LEN = 4;
-const int BRAIN_OUTPUT_LEN = 2;
-const int BRAIN_HIDDEN_LEN = 6;
-const int BRAIN_HIDDEN_LAYER_LEN = 1;
+static int BRAIN_INPUT_LEN = 4;
+static int BRAIN_OUTPUT_LEN = 2;
+static int BRAIN_HIDDEN_LEN = 6;
+static int BRAIN_HIDDEN_LAYER_LEN = 1;
 
 #define WHITE 255,255,255
 #define BLACK 0,0,0
